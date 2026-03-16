e06a39a0df721e00d01e575bf5af1ffb8e5b616e
Adi Cohen <adico1@gmail.com>
2026-03-16 22:49:11 +0200

    צבאות 2026-03-16 22:49:11

--- צפה/עולמות/cloudflare.py
+++ צפה/עולמות/cloudflare.py
@@ -1,6 +1,6 @@
 # cloudflare
 from datetime import datetime
-שעה = "2026-03-16T22:48:29.959965"
+שעה = "2026-03-16T22:48:53.346184"
 פנימי = 'cloudflare'
 חיצוני = 'https://api.cloudflare.com/client/v4'
 סוג = 'חוץ'
--- צפה/עולמות/github.py
+++ צפה/עולמות/github.py
@@ -1,6 +1,6 @@
 # github
 from datetime import datetime
-שעה = "2026-03-16T22:48:29.959627"
+שעה = "2026-03-16T22:48:53.345843"
 פנימי = 'github'
 חיצוני = 'https://api.github.com'
 סוג = 'חוץ'
